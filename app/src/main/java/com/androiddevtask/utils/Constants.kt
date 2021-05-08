254d8ecc150ffb632197b6fc2262a9ec9b5dfb52
Ranjeet Chouhan <ranuluhar@gmail.com>
2021-05-09 00:09:14 +0530

    Added

--- app/src/main/java/com/androiddevtask/utils/Constants.kt
+++ app/src/main/java/com/androiddevtask/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.androiddevtask.utils
 object Constants {
 
     //on HTTP 401 Bad token please regenrate new token from Github Profile
-    val AUTH_TOKEN = "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"
+    val AUTH_TOKEN = "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"
     val USER_RESPONSE = "USER_RESPONSE"
     val BASE_URL = "https://api.github.com/"
 }
